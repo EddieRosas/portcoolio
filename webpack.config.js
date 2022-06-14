541be15138281cbdc20b986b451427a8a1e418f0
Eddie Rosas <eddie@hfa3.org>
2022-06-13 22:35:27 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: { static: path.join(__dirname, "dist") },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader'], // evaluated right to left, so ts-loader first
+            },
+        
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+};
