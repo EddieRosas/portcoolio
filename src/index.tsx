0aa8ad20fadd50b137be358a207abd840171dbd9
Eddie Rosas <eddie@hfa3.org>
2022-06-14 14:08:32 -0700

    catch missing root element/container

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const container: HTMLElement = document.getElementById('root')!;
+const container = document.getElementById('root');
+if (!container) throw new Error("Failed to find root container");
 const root = createRoot(container)
 
 root.render(<App />);
